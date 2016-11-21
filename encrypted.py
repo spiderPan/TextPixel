f49e736ad70ad1320dc93168b099cb6bd97c7f96
pan <bpan@northern.co>
2016-11-21 15:55:33 -0500

    removed if primer detection

--- encrypted.py
+++ encrypted.py
@@ -2,16 +2,9 @@ from PIL import Image
 import math
 
 
-def is_prime(a):
-    return all(a % i for i in xrange(2, a))
-
-
 def find_max_factor(a):
-    if is_prime(a):
-        return a
-    else:
-        factor_list = [i for i in xrange(2, a) if a % i == 0]
-        return factor_list[int(math.floor(len(factor_list) / 2))]
+    factor_list = [i for i in xrange(1, a + 1) if a % i == 0]
+    return factor_list[int(math.floor(len(factor_list) / 2))]
 
 with open('./info.txt', 'r') as myfile:
     txt = myfile.read().replace('\n', '')
