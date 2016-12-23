a2b4f4d7718809fee49dd826602982231fc5919d
pan <bpan@northern.co>
2016-12-23 09:00:27 -0500

    added onload event

--- decrypted.js
+++ decrypted.js
@@ -2,10 +2,12 @@ var myImg = new Image(),
     canvas = document.createElement('canvas'),
     context = canvas.getContext('2d'),
     ascList = [];
-myImg.src = '/wp-content/themes/northern/images/puzzle.png';
-context.drawImage(myImg, 0, 0);
-ascList = context.getImageData(0, 0, myImg.width, myImg.height).data.filter(function(value, index) {
-    return (index + 1) % 4 !== 0;
-});
 
-String.fromCharCode.apply(null, ascList);
+myImg.onload = function() {
+    context.drawImage(myImg, 0, 0);
+    ascList = context.getImageData(0, 0, myImg.width, myImg.height).data.filter(function(value, index) {
+        return (index + 1) % 4 !== 0;
+    });
+    console.log(String.fromCharCode.apply(null, ascList));
+}
+myImg.src = '/wp-content/themes/northern/images/puzzle.png';
